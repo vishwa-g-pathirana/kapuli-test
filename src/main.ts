576f84890794720d2ae5667b428b47993f8fd74e
Kumuditha Bandara <kumuditha@nimidev.com>
2025-10-20 12:12:17 +0530

    Implemented Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('Kapuli API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
